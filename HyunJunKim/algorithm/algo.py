6c7251b08bd48ba468554066c35e3299b1c38450
hj.kim <1q2w3e4r5t^>
2020-11-04 23:55:17 +0900

    ## hj.kim

---
+++ HyunJunKim/algorithm/algo.py
@@ -0,0 +1,19 @@
+
+#
+print('Hello World!')
+
+## we love kriii
+
+for i in range(2):
+    print("강한친구 대한육군")
+
+## 고양이 이걸왜그려 ㅡㅡ
+print("\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+
+
+## a+b
+a, b = input().split()
+print(int(a)+int(b))
